da418a8e21d29892e91edbff6e774f332ac83fbb
Henrique Bonatto <nattohen@gmail.com>
2024-04-16 11:29:21 -0300

    finalizando styles do login

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import Header from "./components/macro/global/header/Header"
 import Banner from "./components/macro/home/Banner/Banner"
+import Heading from "./components/micro/Heading/Heading"
 
 function App() {
   return (
     <>
       <Header view="normal"/>
       <Banner />
+
+      <Heading level={1}>Ultimos Lançamentos</Heading>
     </>
-    
   )
 }
 
