f6657dac4349881c8f497c6833096058a090f004
Pedro Lucas Dall' Igna <pedro.lucas-di@hotmail.com>
2024-05-30 23:22:52 -0300

    feat: makes use of token for requests

--- src/services/http.service.ts
+++ src/services/http.service.ts
@@ -1,14 +1,18 @@
 import axios, { AxiosInstance } from "axios";
 import { API_URL } from "./constants";
+import store from "../store";
 
 let http: AxiosInstance;
 
 if (API_URL) {
+    const SESSION_TOKEN = store.getState().general.sessionToken
+
     http = axios.create({
         baseURL: API_URL,
         headers: {
             Accept: "application/json",
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${SESSION_TOKEN}`
         }
     });
 } else {
